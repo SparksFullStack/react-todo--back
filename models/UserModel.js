a0b8e4f479d76b6c47c165da58a0922317b29ab9
SparksFullStack <csparks@antioch.edu>
2019-03-15 18:13:37 -0700

    user model created and register endpoint working

---
+++ models/UserModel.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+const { ObjectId } = mongoose.Schema.Types;
+
+
+const UserModel = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4,
+    },
+    securityQuestionAnswer: {
+        type: String,
+        required: true
+    },
+    todoList: [{ type: ObjectId, ref: 'ListModel' }]
+});
+
+UserModel.pre('save', function(next) {
+    return bcrypt
+      .hash(this.password, salt)
+      .then(hash => {
+        this.password = hash;
+        return next();
+      })
+      .catch(err => {
+        return next(err);
+      });
+});
+
+UserModel.pre('save', function(next) {
+    return bcrypt
+      .hash(this.securityQuestionAnswer, salt)
+      .then(hash => {
+        this.securityQuestionAnswer = hash;
+        return next();
+      })
+      .catch(err => {
+        return next(err);
+      });
+});
+
+// UserModel.pre('save', function(next) {
+//     const passHash = bcrypt.hashSync(this.password, salt);
+//     const secHash = bcrypt.hashSync(this.securityQuestionAnswer, salt);
+//     this.password = passHash;
+//     this.securityQuestionAnswer = secHash;
+//     return;
+// });
+
+// UserModel.methods.validatePassword = function(passwordGuess) {
+//     return bcrypt.compare(passwordGuess, this.password);
+// };
+
+// UserModel.methods.validateSecQuestion = function(questionGuess) {
+//     return bcrypt.compare(questionGuess, this.securityQuestionAnswer);
+// };
+
+module.exports = mongoose.model('users', UserModel);
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,10 +1,30 @@
 // this router handles all things related to creating accounts and authentication
+// * TODO
+// create user route
+// sign in route
+// sign out route
+// forgot password route
+
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const JWT = require('jsonwebtoken');
+const UserModel = require('../models/UserModel');
+const { JWT_SECRET } = process.env;
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('The auth router is working!'));
+
+router.post('/register', (req, res) => {
+    const { username, password, securityQuestionAnswer } = req.body;
+    const newUser = new UserModel({ username, password, securityQuestionAnswer });
+    const { _id } = newUser;
+    
+    newUser.save(err => {
+        if (err) return res.status(500).json({ registerError: "There was an error when trying to register you, please try again"});
+        
+    })
+})
+
 
 module.exports = router;
