4eb2d16113b9701b3ee264c3a89b9ab8a6f66d2a
SparksFullStack <csparks@antioch.edu>
2019-03-15 17:37:30 -0700

    mongo database added and connected to on mlab

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const server = express();
 const port = process.env.PORT || 3001;
@@ -7,6 +8,13 @@ const port = process.env.PORT || 3001;
 mongoose.promise = global.Promise;
 const databaseOptions = { useNewUrlParser: true };
 mongoose.set('useCreateIndex', true);
+
+console.log(process.env.MLAB_URI)
+
+mongoose.connect(process.env.MLAB_URI, databaseOptions);
+mongoose.connection
+    .once('open', () => console.log('The database is connected'))
+    .on('error', (err) => console.warn(err));
 
 server.use(express.json());
 
