5932263fbb497d584ccd78016aea0082f86a2d2d
SparksFullStack <csparks@antioch.edu>
2019-03-17 16:48:36 -0700

    login route finished

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -27,7 +27,24 @@ router.post('/register', (req, res) => {
             else res.status(200).json({ JWT: token });
         })
     })
-})
+});
+
+router.post('/login', (req, res) => {
+    const credentials = req.body;
+
+    UserModel.findOne({ email: credentials.email })
+        .then(userRecord => {
+            if (!userRecord) return res.status(404).json('No user with that email was found');
+
+            const { email, password, securityQuestionAnswer } = userRecord;
+            if (bcrypt.compareSync(credentials.password, password)) {
+                JWT.sign({ email, password, securityQuestionAnswer }, JWT_SECRET, { expiresIn: "6hr", algorithm: 'HS256' }, (err, decoded) => {
+                    if (err) return res.status(500).json({ loginError: "There was an error when trying to generate a JWT for the user" });
+                    res.status(200).json({ JWT: decoded });
+                });
+            } else res.status(401).json({ loginError: "The password provided didn't match the user record, please try again" });
+        });
+});
 
 
 module.exports = router;
